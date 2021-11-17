89480c4327db94fd3e8be090be5a171f0241b530
ai2025 <aisyatush11@gmail.com>
2021-11-18 01:22:37 +0700

    raw frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {GoogleLogin, GoogleLogout} from "react-google-login";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [showloginButton, setShowloginButton] = useState(true);
+	const [showlogoutButton, setShowlogoutButton] = useState(false);
+
+	// const [loginData, setLoginData] = useState(
+	// 	localStorage.getItem("loginData")
+	// 		? JSON.parse(localStorage.getItem("loginData"))
+	// 		: null
+	// );
+
+	const handleFailure = (result) => {
+		alert(result);
+	};
+
+	const handleLogin = (result) => {
+		console.log(result);
+		setShowloginButton(false);
+		setShowlogoutButton(true);
+	};
+
+	// const handleLogin = async (googleData) => {
+	// 	// console.log(googleData);
+	// 	const result = await fetch("/api/google-login", {
+	// 		method: "POST",
+	// 		body: JSON.stringify({
+	// 			token: googleData.tokenId,
+	// 		}),
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 	});
+
+	// 	const data = await result.json();
+	// 	setLoginData(data);
+	// 	localStorage.setItem("loginData", JSON.stringify(data));
+	// };
+
+	const handleLogout = () => {
+		// localStorage.removeItem("loginData");
+		// setLoginData(null);
+		alert("You have been logged out");
+		console.clear();
+		setShowloginButton(true);
+		setShowlogoutButton(false);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{showloginButton ? (
+					<div>
+						<h1>Login with OAuth</h1>
+						<GoogleLogin
+							clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+							buttonText="Login with Google"
+							onSuccess={handleLogin}
+							onFailure={handleFailure}
+							// cookiePolicy={"single_host_origin"}
+						></GoogleLogin>
+					</div>
+				) : null}
+
+				{showlogoutButton ? (
+					<div>
+						<h1>Welcome!! You are logged in now</h1>
+						<GoogleLogout
+							clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+							buttonText="Sign out"
+							onLogoutSuccess={handleLogout}
+						></GoogleLogout>
+					</div>
+				) : null}
+			</header>
+		</div>
+	);
 }
 
 export default App;
